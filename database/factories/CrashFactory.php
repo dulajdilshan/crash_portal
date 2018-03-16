d998653fdcecc95475880625df8ca636259c7446
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2018-03-17 01:53:45 +0530

    some faker testing

--- database/factories/CrashFactory.php
+++ database/factories/CrashFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Crash::class, function (Faker $faker) {
     return [
-        //
+        'crash_name'=> $faker->name,
+        'description'=>$faker->text,
+        'uploaded_by'=>$faker->userName,
     ];
 });
