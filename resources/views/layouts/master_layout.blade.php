e1e8bca0e89bb2305fcfe9fb47d73332d913ad13
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2018-03-18 00:13:41 +0530

    first view Crashes

--- resources/views/layouts/master_layout.blade.php
+++ resources/views/layouts/master_layout.blade.php
@@ -1,29 +1,64 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MuseScore</title>
 
+<head>
+    <meta charset="utf-8" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>MuseScore Developer's Portal</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <!-- Bootstrap core CSS     -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="{{asset('css/material-dashboard.css?v=1.2.0')}}" rel="stylesheet" />
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
+<div class="wrapper">
 
-{{--Header--}}
-<div class="header">
-    @yield('layouts.header')
+    {{--  sidebar  --}}
+    <div class="sidebar" data-color="blue" data-image="../assets/img/sidebar-1.jpg">
+        @include('layouts.sidebar')
+    </div>
+
+    <div class="main-panel">
+        
+        @yield('header')
+
+        @yield('content')
+        
+        @yield('layouts.footer')
+        
+    </div>
 </div>
 
-{{--Content--}}
-<div class="content">
-    @yield('content')
-</div>
 
-{{--Footer--}}
-<div class="footer">
-    @yield('layouts.footer')
-</div>
+
 
 </body>
+<!--   Core JS Files   -->
+<script src="../assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/js/material.min.js" type="text/javascript"></script>
+<!--  Charts Plugin -->
+<script src="../assets/js/chartist.min.js"></script>
+<!--  Dynamic Elements plugin -->
+<script src="../assets/js/arrive.min.js"></script>
+<!--  PerfectScrollbar Library -->
+<script src="../assets/js/perfect-scrollbar.jquery.min.js"></script>
+<!--  Notifications Plugin    -->
+<script src="../assets/js/bootstrap-notify.js"></script>
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="../assets/js/material-dashboard.js?v=1.2.0"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="../assets/js/demo.js"></script>
+
 </html>
