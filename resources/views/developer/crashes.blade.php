1a0670de48b72548555b97312dcbc809bac1a4a3
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2018-03-18 00:59:38 +0530

    developer.crashes.blade

--- resources/views/developer/crashes.blade.php
+++ resources/views/developer/crashes.blade.php
@@ -1,23 +1,56 @@
 @extends('layouts.master_layout')
 
-
+@section('header')
+    {{--Header--}}
+    <nav class="navbar navbar-transparent navbar-absolute">
+        @include('layouts.header')
+    </nav>
+@endsection
 
 @section('content')
-    <table>
-        <tr>
-            <th>Crash Name</th>
-            <th>Description</th>
-            <th>Uploaded By</th>
-        </tr>
+        {{--Content--}}
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header" data-background-color="blue">
+                                <h4 class="title">Crashes</h4>
+                                <p class="category">Crashes uploaded by the users</p>
+                            </div>
+                            <div class="card-content table-responsive">
+                                <table class="table">
+                                    <thead class="text-primary" style="color: slategrey">
+                                    <th>Crash Name</th>
+                                    {{--  <th>Description</th>  --}}
+                                    <th>Uploaded By</th>
+                                    <th>Assigned to</th>
+                                    </thead>
+                                    <tbody>
 
-    @foreach($crashes as $crash)
-        <tr>
-            <td>{{$crash->crash_name}}</td>
-            <td>{{$crash->description}}</td>
-            <td>{{$crash->uploaded_by}}</td>
-        </tr>
+                                    @foreach($crashes as $crash)
+                                    <tr>
+                                        <td>{{$crash->crash_name}}</td>
+                                        {{--  <td>{{$crash->description}}</td>  --}}
+                                        <td>{{$crash->uploaded_by}}</td>
+                                        <td class="text-primary">Dulaj</td>
+                                    </tr>
+                                    @endforeach
 
-    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                {{--  Another col  --}}
+                </div>
+            </div> 
+        </div>  
+@endsection
 
-    </table>
+@section('footer')
+    {{--Footer--}}
+    <footer class="footer">
+        @include('layouts.footer')
+    </footer>
 @endsection
