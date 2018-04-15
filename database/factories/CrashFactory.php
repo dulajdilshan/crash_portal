84dc39123567244ef4c7f14e718962412a2a26a2
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2018-04-16 02:50:38 +0530

    Testing Factory Created

--- database/factories/CrashFactory.php
+++ database/factories/CrashFactory.php
@@ -6,6 +6,9 @@ $factory->define(App\Crash::class, function (Faker $faker) {
     return [
         'crash_title'=> $faker->userName,
         'report_created_at'=>$faker->dateTime,
+        'developer_id'=>rand(1,100),
+        'progress'=>rand(0,100),
+        'solved'=>rand(0,1),
 //        =>str_random(10),
 //        'uploaded_by'=>$faker->userName,
     ];
--- database/factories/DeveloperFactory.php
+++ database/factories/DeveloperFactory.php
@@ -4,8 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Developer::class, function (Faker $faker) {
     return [
+        'user_id'=>$faker->unique()->numberBetween(1,100),
         'name'=>$faker->name,
         'email'=>$faker->safeEmail,
-
+        'user_name'=>$faker->userName,
+        'github_url'=>$faker->url,
+        'linkedin_url'=>$faker->url,
+        'fb_url'=>$faker->url,
+        'about'=>str_random(10),
     ];
 });
